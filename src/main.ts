7146c15ead397bb38d1e75d1f1b1f69107f988fb
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-07-03 17:13:17 -0300

    build: ajustando configurações

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+import Equal from "equal-vue";
+import "equal-vue/dist/style.css";
+
+createApp(App)
+    .use(router)
+    .use(Equal)
+    .mount("#app");
