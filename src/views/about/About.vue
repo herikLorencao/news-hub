de0f605ba044a8ae50f7ad245d79ca015d3ae72c
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-07-05 20:45:54 -0300

    feat: adicionando página de sobre

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -1,6 +1,15 @@
 <template>
   <main>
     <h1>Sobre</h1>
+    <p>
+      Aplicação para listagens de notícias utilizando a News API, Vue 3, Sass e
+      o framework de UI Equal.
+    </p>
+    <p>Desenvolvido por Herik dos Santos Lorenção em julho de 2021.</p>
+    <a href="https://github.com/herikLorencao/news-hub" target="_blank"
+      >Link do Código Fonte</a
+    >
+    <p>OBS: Todas as notícias mostradas são de fonte da News API.</p>
   </main>
 </template>
 
@@ -11,4 +20,5 @@ export default defineComponent({});
 </script>
 
 <style lang="scss" scoped>
+@import "about";
 </style>
